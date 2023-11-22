74c45ba59eb2c9c7cccb9d06b076a298c6cc4fbe
xiaosongfu <xiaosong.fu@outlook.com>
2023-11-22 18:03:52 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+```
+npm install
+npm run start
+```
+
+> Deploy operation is automatic with Vercel CICD.
---
+++ api/index.ts
@@ -0,0 +1,41 @@
+import { Hono, Context } from "hono";
+import { handle } from "hono/vercel";
+import { sql } from "@vercel/postgres";
+
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono();
+
+app.get("/", async (c: Context) => {
+  // console.log(c.req.url);
+  // https://usdt.abiui.xyz/
+  // https://0x123456.mainnet.abiui.xyz/
+  const url = c.req.url;
+  const dest = url.slice(url.indexOf("/") + 2, url.indexOf(".abi.xyz") - 10);
+  // console.log("dest: ", dest);
+  // dest: usdt
+  // dest: 0x123456.mainnet
+
+  const contractAndNetwork = dest.split(".");
+  if (contractAndNetwork.length === 2) {
+    const { rows } =
+      await sql`SELECT * FROM public."contracts" WHERE contract=${contractAndNetwork[0]} AND network=${contractAndNetwork[1]}`;
+    if (rows.length === 0) {
+      return c.notFound();
+    }
+
+    return c.html(rows[0].html);
+  } else {
+    const { rows } =
+      await sql`SELECT * FROM public."contracts" WHERE alias=${dest}`;
+    if (rows.length === 0) {
+      return c.notFound();
+    }
+
+    return c.html(rows[0].html);
+  }
+});
+
+export default handle(app);
